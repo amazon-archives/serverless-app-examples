21c35760421bcf12934ab16a6087c38b2eea2b47
Wujing Yao <wujiny@amazon.com>
2019-12-18 14:52:18 -0800

    Upgrade nodejs version for nodejs apps

--- javascript/ses-notification-nodejs/index.js
+++ javascript/ses-notification-nodejs/index.js
@@ -2,7 +2,7 @@
 console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
-    //console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('Received event:', JSON.stringify(event, null, 2));
     const message = JSON.parse(event.Records[0].Sns.Message);
     
     switch(message.notificationType) {
